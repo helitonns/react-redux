6110869b1c13a8e5aaff28f7e6c628ebf18d721c
helitonns <helitonns@gmail.com>
2024-02-03 19:04:47 -0400

    Aula 0502

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import App from "./04/08/App";
-import store from "./04/08/store/configureStore";
+import App from "./05/02/App";
+import store from "./05/02/store/configureStore";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  
     <Provider store={store}>
       <App />
     </Provider>
